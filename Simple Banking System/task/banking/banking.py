6c2c578e73e65ac9042c30b08fca4598be22bd85
Nishant <nishantchauhan949@hotmail.com>
2020-11-15 16:39:23 +0530

    Implemented Luhn Algo

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -11,7 +11,7 @@ class Customer:
     def __init__(self):
         self.card_pin = randint(1000, 9999)
         self.account_number = self.account_number_generator()
-        self.checksum = randint(0, 9)
+        self.checksum = 0
         self.card_number = self.card_number_generator()
         self.balance = 0
         self.account_details = [self.card_number, self.card_pin, self.balance]
@@ -29,10 +29,42 @@ class Customer:
         return new_acc_num
 
     def card_number_generator(self):
-        string_card_number = str(self.issuer_identification_number) + \
-                             str(self.account_number) + str(self.checksum)
+        return self.luhn_implementation()
 
-        return int(string_card_number)
+    def luhn_implementation(self):
+        # card number without checksum
+        string_card_number = str(self.issuer_identification_number) + \
+                          str(self.account_number)
+
+        list_card_number = []
+
+        for number in string_card_number:
+            list_card_number.append(int(number))
+
+        card_number_by_2 = list_card_number.copy()
+
+        for index in range(0, len(card_number_by_2), 2):
+            card_number_by_2[index] *= 2
+
+        for index in range(0, len(card_number_by_2)):
+            if card_number_by_2[index] > 9:
+                card_number_by_2[index] -= 9
+
+        add_all_numbers = 0
+        for num in card_number_by_2:
+            add_all_numbers += num
+
+        if add_all_numbers % 10 != 0:
+            self.checksum = 10 - (add_all_numbers % 10)
+
+        list_card_number.append(self.checksum)
+
+        some_string = ''
+        for num in list_card_number:
+            some_string += str(num)
+
+        final_card_number = int(some_string)
+        return final_card_number
 
 
 def main():
---
+++ Simple Banking System/task/banking/testing.py
@@ -0,0 +1,7 @@
+stri = 'cba334'
+
+number = 49883394
+
+print(list(stri))
+
+print((list(number)))
